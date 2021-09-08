d861a6338cba77586e326e25251eb0ed61c5e23d
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-08 19:43:00 -0400

    Exercicios de Estrutura Condicional realizados

---
+++ ExerciciosEstruturaCondicional/Exercicio 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_1 {
+    class Program {
+        static void Main(string[] args) {
+            //Exercicio 1) Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+            //Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+            //ordem crescente ou decrescente.
+
+            Console.Write("Afim de saber se os números são multiplos, digite DOIS NUMEROS(X e Y) respectivamente separados por um ESPAÇO: ");
+            String[] numeros = Console.ReadLine().Split(' ');
+
+            double x = double.Parse(numeros[0]);
+            double y = double.Parse(numeros[1]);
+
+            if (x % y == 0  || y % x == 0 ) {
+                Console.WriteLine("São multiplos!");
+            } else {
+                Console.WriteLine("Não são multiplos!");
+            }
+
+        }
+    }
+}
---
+++ ExerciciosEstruturaCondicional/Exercicio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_2 {
+    class Program {
+        static void Main(string[] args) {
+            //Exercicio 2) Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+            //de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+            //ponto, ou se está sobre um dos eixos cartesianos ou na origem(x = y = 0).
+            //Se o ponto estiver na origem, escreva a mensagem “Origem”.
+            //Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+            //situação.
+
+            Console.Write("Digite a coordenada 'x': ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Digite a coordenada 'y': ");
+            double y = double.Parse(Console.ReadLine());
+
+            if (x > 0.0 && y > 0.0) {
+                Console.WriteLine("Quadrante: Q1");
+            } else if (x < 0.0 && y > 0.0) {
+                Console.WriteLine("Quadrante: Q2");
+            } else if (x < 0.0 && y < 0.0) {
+                Console.WriteLine("Quadrante: Q3");
+            } else if (x > 0.0 && y < 0.0) {
+                Console.WriteLine("Quadrante: Q4");
+            } else {
+                Console.WriteLine("Origem");
+            }
+
+        }
+    }
+}
