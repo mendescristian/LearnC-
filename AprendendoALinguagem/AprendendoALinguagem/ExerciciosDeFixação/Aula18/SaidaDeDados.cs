81b5d26baa2b8315e71739c536342a623b847aab
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-08 11:37:31 -0400

    Primeiro Exercicio Realizado

---
+++ AprendendoALinguagem/AprendendoALinguagem/ExerciciosDeFixação/Aula18/SaidaDeDados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AprendendoALinguagem.ExerciciosDeFixação.Aula18 {
+    class SaidaDeDados {
+        static void Main(string[] args) {
+
+            //Declaração de variáveis:
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            int idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            //Construindo estrutura:
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é R${preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é R${preco2:F2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondando (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
