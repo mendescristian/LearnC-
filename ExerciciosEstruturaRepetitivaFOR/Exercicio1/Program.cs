92d7d0fd1de9e3461fae4865fe55f6409371572d
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-09 11:41:33 -0400

    Exercicios de estrutura repetitiva FOR finalizados

---
+++ ExerciciosEstruturaRepetitivaFOR/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            //Exercicio 1) Leia um valor inteiro X(1 <= X <= 1000).Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+            //X, se for o caso.
+
+            Console.Write("Digite um número para saber quais são os números ímpares até ele: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++) {
+                if ( i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+} 
---
+++ ExerciciosEstruturaRepetitivaFOR/Exercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+            //Exercicio 2) Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+            //começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+            //exemplo.
+
+            Console.Write("Digite um limite de números:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                int qd1 = (int) Math.Pow(i , 2);
+                int qd2 = (int)Math.Pow(i, 3);
+                Console.WriteLine($"{i} ao quadrado = {qd1} e ao cubo = {qd2}");
+
+            }
+
+        }
+    }
+}
