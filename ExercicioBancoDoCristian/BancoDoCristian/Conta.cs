4c434aa3fe2da4d917801903fa5825348e0b1264
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-10 10:38:44 -0400

    Feito programa de um banco na qual permite apenas alteração de saldo através de SAQUES ou DEPÓSITOS, e cada SAQUE possui uma taxa de R$5,00

---
+++ ExercicioBancoDoCristian/BancoDoCristian/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoDoCristian {
+    class Conta {
+
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+        public string Titular { get; set; }
+
+        public Conta(int numeroConta, double saldo, string titular) {
+            NumeroConta = numeroConta;
+            Saldo = saldo;
+            Titular = titular;
+        }
+
+        public Conta(int numeroConta, string titular) {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+
+        public void Deposita(double valor) {
+            if (valor > 0.0) {
+                Saldo += valor;
+            } else {
+                Console.WriteLine("O valor a ser depositado deverá ser maior que R$0,00");
+            }
+        }
+
+        public void Saca(double valor) {
+            if (valor > 0.0 || valor >= Saldo) {
+                Saldo -= (valor + 5.0);
+            } else {
+                Console.WriteLine("O valor a ser sacado deverá ser maior que R$00,01 e menor que o saldo atual.");
+            }
+        }
+
+        public override string ToString() {
+            return $"Nome do Titular: {Titular} | Número da conta: {NumeroConta} | Saldo atual: R${Saldo:f2}";
+        }
+
+    }
+}
---
+++ ExercicioBancoDoCristian/BancoDoCristian/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BancoDoCristian {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Conta conta;
+            Console.WriteLine("----------------Banco do Cristian------------------");
+
+            Console.Write("Digite o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string nomeTit = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? [S/N]: " );
+            char havDepos = char.Parse(Console.ReadLine());
+            
+
+            if (havDepos == 'S' || havDepos == 's') {
+                Console.Write("Digite o valor de depósito: R$");
+                double deposInicial = double.Parse(Console.ReadLine());
+
+                conta = new Conta(numConta, deposInicial, nomeTit);
+            } else {
+                conta = new Conta(numConta, nomeTit);
+            }
+
+            Console.WriteLine("--------Dados da conta-------");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para depósito: R$");
+            double valor = double.Parse(Console.ReadLine());
+            conta.Deposita(valor);
+
+            Console.WriteLine("--------Dados da conta-------");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: R$");
+            valor = double.Parse(Console.ReadLine());
+            conta.Saca(valor);
+
+            Console.WriteLine("--------Dados da conta-------");
+            Console.WriteLine(conta);
+
+
+
+        }
+    }
+}
