5891c805db5fde055cdec4791d4fb765b3226abb
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-09 21:16:36 -0400

    Exercicio finalizado.

---
+++ ExercicioSobreClasse/ExercicioFuncionario/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFuncionario {
+    class Funcionario {
+
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public void AumentaSalario(double porc) {
+            salarioBruto += salarioBruto * (porc / 100.0);
+        }
+
+        public override string ToString() {
+            return $"Funcionário: {nome} | Salário Bruto: R${salarioBruto} | Imposto: R${imposto}";
+        }
+    }
+}
---
+++ ExercicioSobreClasse/ExercicioFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExercicioFuncionario {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome do Funcionário: ");
+            funcionario.nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.salarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(funcionario);
+
+            Console.Write("Você deseja aumentar o salário do funcionário? [S/N]: ");
+            char escolha = char.Parse(Console.ReadLine());
+
+            if (escolha == 'S') {
+                Console.Write("Digite a porcentagem do aumento: ");
+                funcionario.AumentaSalario(double.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(funcionario);
+            Console.WriteLine("Obrigado pela preferencia!");
+
+
+        }
+    }
+}
