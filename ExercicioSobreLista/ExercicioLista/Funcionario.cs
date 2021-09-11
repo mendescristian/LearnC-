3788210477785c350c94758193493713da70e3c7
Cristian Mendes <cristianpmendes@gmail.com>
2021-09-11 17:19:41 -0400

    Exercicio realizado sobre uma lista de funcionários

---
+++ ExercicioSobreLista/ExercicioLista/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioLista {
+    class Funcionario {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncrementarSalario(double porc) {
+            Salario += (Salario * (porc / 100.0));
+            Console.WriteLine("Salário atualizado!");
+        }
+
+        public override string ToString() {
+            return $"Id do funcionário: {Id} | Nome do funcionário: {Nome} | Salário do funcionário: R${Salario}";
+        }
+    }
+}
---
+++ ExercicioSobreLista/ExercicioLista/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioLista {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> lista = new List<Funcionario>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write($"Id do funcionário #{i}: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write($"Nome do Funcionário #{i}: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Salário do funcionário #{i}: R$");
+                double salario = double.Parse(Console.ReadLine());
+
+                lista.Add(new Funcionario(id, nome, salario));
+            }
+
+            Console.Write("Deseja visualizar a lista de funcionários? [S/N]: ");
+            char respo = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            if(respo == 'S' || respo == 's') {
+                Console.WriteLine("--------------------Lista de Funcionarios------------------");
+                foreach (Funcionario func in lista) {
+                    Console.WriteLine(func);
+                }
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+
+            Console.Write("Deseja aumentar o salário de algum funcionário? [S/N]: ");
+            char respAum = char.Parse(Console.ReadLine());
+            if (respAum == 'S' || respAum == 's') {
+                Console.Write("Digite o ID do funcionário desejado: ");
+                int searchID = int.Parse(Console.ReadLine());
+                Funcionario achaId = lista.Find(func => func.Id == searchID);
+                if (achaId != null) {
+                    Console.Write("Digite a porcentagem de aumento: ");
+                    double porc = double.Parse(Console.ReadLine());
+                    achaId.IncrementarSalario(porc);
+                }
+
+                Console.WriteLine("--------------------Lista de Funcionarios------------------");
+                foreach (Funcionario func in lista) {
+                    Console.WriteLine(func);
+                }
+                Console.WriteLine("-----------------------------------------------------------");
+
+
+            }
+
+
+
+        }
+    }
+}
